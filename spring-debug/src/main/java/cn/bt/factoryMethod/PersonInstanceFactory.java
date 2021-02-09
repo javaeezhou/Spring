d46228ac774fc09745b18e185ed1dc2a72c73eca
zhouqian <zhouqian@hzzl.net>
2021-02-09 18:38:04 +0800

    加点注释

---
+++ spring-debug/src/main/java/cn/bt/factoryMethod/PersonInstanceFactory.java
@@ -0,0 +1,10 @@
+package cn.bt.factoryMethod;
+
+public class PersonInstanceFactory {
+    public Person getPerson(String name){
+        Person person = new Person();
+        person.setId(1);
+        person.setName(name);
+        return person;
+    }
+}
